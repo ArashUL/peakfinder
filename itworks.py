82aef8446e3126c32a9ee75353c04147059b2c98
arash <tebyani@physics.leidenuniv.nl>
2018-11-09 13:30:30 +0100

    added final comments to the final code

--- itworks.py
+++ itworks.py
@@ -2,7 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 
-haxis = np.linspace(0,10*np.pi,1001)            # horizental axis array including 1001 points
+# Parameters
+NN = 1001            #the number of data points
+# other parameters are SegNum and the Filter variable
+
+haxis = np.linspace(0,10*np.pi,NN)            # horizontal axis array including 1001 points
 
 # defining a few basic signals
 sig1 = np.sin (haxis)
@@ -18,7 +22,7 @@ cleansig = sig1 + sig2 + sig3 + sig4 + sig5 + sig6
 
 #Adding NOISE
 nf = 1.4   # noise factor to adjust noise strength
-noisesig = nf * np.random.random_sample(1001)
+noisesig = nf * np.random.random_sample(NN)
 
 totalsigi = cleansig + noisesig
 
@@ -28,16 +32,16 @@ plt.plot(haxis, totalsigi, linewidth = 0.3 , color='g')
 
 #Removal of high frequency noise by using a filter
 from scipy.signal import savgol_filter 
-totalsig = savgol_filter(totalsigi, 1001, 18)           # the strength of filter can be adjusted with the last number
+totalsig = savgol_filter(totalsigi, NN, 18)           # the strength of filter can be adjusted with the last number
 plt.plot(haxis, totalsig, linewidth = 0.7, color='k')  # high frequency noise removed
 
 
 med = np.median (totalsig)
-medarray = np.ones(1001) * med        # defining a signal whose value is equal to the median of the main signal everywhere
+medarray = np.ones(NN) * med        # defining a signal whose value is equal to the median of the main signal everywhere
 plt.plot(haxis, medarray)
 
 
-totalsig2 = np.zeros(1001)
+totalsig2 = np.zeros(NN)
 for i in range(len(haxis)):             # defining a signal with only the values of totalsig above the median: in effect, dividing the signal into zero and nonzero segments
     if totalsig[i] > med:
         totalsig2 [i] = totalsig[i]
@@ -48,7 +52,7 @@ for i in range(len(haxis)):             # defining a signal with only the values
 #putting each segment of the signal between zeros segments into a separate row of a matrix
 counter = 0      # to keep track of matrix row
 SegNum = 100     # the number we assume for the amount of signal segments between zero segments
-M1 = np.zeros((SegNum,1001))
+M1 = np.zeros((SegNum,NN))
 for i in range(len(haxis)):
     if totalsig2[i] != totalsig2[i-1]:    #to discard the zero segments
         if totalsig2[i] == 0:
@@ -70,7 +74,7 @@ for i in range(SegNum):
         lmy [i] = max(M1[i,:])
         for b in range (1001):        # to find which column the maximum value belongs to 
             if M1[i,b] == max(M1[i,:]):
-                lmx[i] = haxis[b]        # to keep track of the actual horizental value 
+                lmx[i] = haxis[b]        # to keep track of the actual horizontal value 
         
 #discarding redundant rows with (0,0) coordinates by creating two new lists
 xfin = [lmx[0]]
